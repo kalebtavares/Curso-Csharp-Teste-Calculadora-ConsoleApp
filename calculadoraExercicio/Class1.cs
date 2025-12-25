b68d6ee33387554212582005025c90214143d830
kalebtavares <kalebtavares@icloud.com>
2025-12-25 09:46:10 -0400

    Correção na mensagem de boas vindas.

---
+++ calculadoraExercicio/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadoraExercicio
+{
+    public class Calculadora
+    {
+        public void Executar()
+        {
+            
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Seja Bem vindo a nossa calculadora!!");
+                Console.WriteLine("Digite o primeiro valor:");
+                string? primeiroValorString = Console.ReadLine();
+                double primeiroValor = Convert.ToDouble(primeiroValorString);
+
+                Console.WriteLine("Digite o segundo valor:");
+                string? segundoValorString = Console.ReadLine();
+                double segundoValor = Convert.ToDouble(segundoValorString);
+
+                Console.WriteLine($"Os valores digitados são : {primeiroValor} e {segundoValor}");
+
+
+
+                Console.WriteLine("Selecione a operação desejada");
+                Console.WriteLine("+" + " Soma");
+                Console.WriteLine("-" + " Subtracao");
+                Console.WriteLine("*" + " Multiplicacao");
+                Console.WriteLine("/" + " Divisao");
+                var operacaoSelecionada = Console.ReadLine();
+
+                //Realizar calculo
+
+                double resultadoOperacao;
+                switch (operacaoSelecionada)
+                {
+                    case "+":
+                        resultadoOperacao = primeiroValor + segundoValor;
+                        Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
+                        break;
+                    case "-":
+                        resultadoOperacao = primeiroValor - segundoValor;
+                        Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
+                        break;
+                    case "*":
+                        resultadoOperacao = primeiroValor * segundoValor;
+                        Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
+                        break;
+                    case "/":
+                        resultadoOperacao = primeiroValor / segundoValor;
+                        Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
+                        break;
+                    default:
+                        Console.WriteLine("Operação inválida");
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Deseja realizar outra operação?");
+                Console.WriteLine("(S) Sim (N) Não");
+                Console.WriteLine();
+            } while (Console.ReadKey().Key == ConsoleKey.S);
+            Console.Clear();
+
+            Console.WriteLine("Até Logo!!");
+        }
+    }
+}
--- calculadoraExercicio/Program.cs
+++ calculadoraExercicio/Program.cs
@@ -1,64 +1,9 @@
-﻿using System.ComponentModel.Design;
+﻿using calculadoraExercicio;
 
-Console.WriteLine("Seja Bem vindo a nossa calculadora");
-
-
-
-do
-{
-    Console.Clear();
-    Console.WriteLine("Digite o primeiro valor:");
-    string? primeiroValorString = Console.ReadLine();
-    double primeiroValor = Convert.ToDouble(primeiroValorString);
-
-    Console.WriteLine("Digite o segundo valor:");
-    string? segundoValorString = Console.ReadLine();
-    double segundoValor = Convert.ToDouble(segundoValorString);
-
-    Console.WriteLine($"Os valores digitados são : {primeiroValor} e {segundoValor}");
-
-    
-
-    Console.WriteLine("Selecione a operação desejada");
-    Console.WriteLine("+" + " Soma");
-    Console.WriteLine("-" + " Subtracao");
-    Console.WriteLine("*" + " Multiplicacao");
-    Console.WriteLine("/" + " Divisao");
-    var operacaoSelecionada = Console.ReadLine();
-
-    //Realizar calculo
-
-    double resultadoOperacao;
-    switch (operacaoSelecionada)
-    {
-        case "+":
-            resultadoOperacao = primeiroValor + segundoValor;
-            Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
-            break;
-        case "-":
-            resultadoOperacao = primeiroValor - segundoValor;
-            Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
-            break;
-        case "*":
-            resultadoOperacao = primeiroValor * segundoValor;
-            Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
-            break;
-        case "/":
-            resultadoOperacao = primeiroValor / segundoValor;
-            Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
-            break;
-        default:
-            Console.WriteLine("Operação inválida");
-            break;
-    }
-        Console.WriteLine();
-    Console.WriteLine("Deseja realizar outra operação?");
-    Console.WriteLine("(S) Sim (N) Não");
-    Console.WriteLine();
-} while (Console.ReadKey().Key == ConsoleKey.S);
-Console.Clear();
-
-Console.WriteLine("Até Logo!!");
+Console.WriteLine("Seja Bem vindo a nossa calculadora!!");
+Console.WriteLine();
+Calculadora calculadora = new Calculadora();
+calculadora.Executar();
 
 
-    Console.ReadKey();
+Console.ReadKey();
